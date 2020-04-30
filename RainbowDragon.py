e8cc450a94e6f4ba8da79e95a35b2358eca503bf
Awnon Bhowmik <awnonbhowmik@users.noreply.github.com>
2020-04-30 17:28:42 -0400

    Create RainbowDragon.py

---
+++ RainbowDragon.py
@@ -0,0 +1,40 @@
+import turtle
+from time import time
+
+ob=turtle.Turtle()
+wn=turtle.Screen()
+wn.bgcolor("black")
+ob.speed('fastest')
+ob.hideturtle()
+
+ob.goto(0,0)
+ob.tracer(n=10,delay=None)
+
+colors = ["violet","indigo","blue","light green","yellow","orange","red"]
+
+def dragoncurve(ob,l,n):
+  for i in range(1):
+    def x(n):
+            if n==0:
+                    return
+            x(n-1)
+            ob.right(90)
+            y(n-1)
+            ob.forward(l)
+    def y(n):
+            if n==0:
+                    return
+            ob.forward(l)
+            x(n-1)
+            ob.left(90)
+            y(n-1)
+    ob.fd(l)
+    x(n)
+
+for i in range(len(colors)):
+  ob.pencolor(colors[i])
+  ob.setheading(360/7*i)
+  dragoncurve(ob,2,12)
+  ob.penup()
+  ob.goto(0,0)
+  ob.pendown()
